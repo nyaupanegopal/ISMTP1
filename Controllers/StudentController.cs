ec21d013339721f01b3c406943fc8a637134dc93
Gopal.Nyaupane <gopal.nyaupane100@gmail.com>
2023-04-10 08:01:20 +0545

    studenet register

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagementSystemNew.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/StudentRegister.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentManagementSystemNew.Models
+{
+    public class StudentRegister
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int Gender { get; set; }
+        public int Department { get; set; }
+        public string[] OptionalSub { get; set; }
+        public string Message { get; set; }
+    }
+}
